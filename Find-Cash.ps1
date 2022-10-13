d1a11a47da4faa74889c22f061351564055564ec
Roel Kemp <42418205+RocknRolo@users.noreply.github.com>
2022-10-13 16:47:03 +0200

    Removed ternary expression for compatibility with PS5

--- Find-Cash.ps1
+++ Find-Cash.ps1
@@ -1,11 +1,17 @@
 function roundTo5 {
     param($amount)
     $diff = $amount % 5;
-    return $amount = $diff -gt 2 ? $amount + (5 - $diff) : $amount - $diff;
+    if ($diff -gt 2) {
+        return $amount + (5 - $diff);
+    } 
+    return $amount - $diff;
 }
 
 function intToCash {
     param([String]$num)
+    if ($num.Length -le 2) {
+        return "0." + $num;
+    }
     return $num.Insert($num.length - 2, '.');
 }
 
@@ -18,18 +24,23 @@ function cashToInt {
     return -1;
 }
 
-$customerPays = Get-Random 500, 1000, 2000, 5000, 10000, 20000;
-$customerOwes = Get-Random -Min 5 -Max $customerPays;
-$correct = roundTo5($customerPays - $customerOwes);
 
-Write-Host "Customer pays with  : € $(intToCash($customerPays))";
-Write-Host "Customer has to pay : € $(intToCash($customerOwes))";
-Write-Host "Customer's change   : € " -NoNewline;
+function question {
+    $customerPays = Get-Random 500, 1000, 2000, 5000, 10000, 20000;
+    $customerOwes = Get-Random -Min 5 -Max $customerPays;
+    $correct = roundTo5($customerPays - $customerOwes);
 
-[String]$answer = roundTo5(cashToInt(Read-Host));
+    Write-Host "Customer pays with  : Euro $(intToCash($customerPays))";
+    Write-Host "Customer has to pay : Euro $(intToCash($customerOwes))";
+    Write-Host "Customer's change   : Euro " -NoNewline;
 
-if ($answer -eq [String]$correct) {
-    Write-Host "Correct!";
-} else {
-    Write-Host "Incorrect!";
+    [String]$answer = roundTo5(cashToInt(Read-Host));
+
+    if ($answer -eq [String]$correct) {
+        Write-Host "Correct!";
+    } else {
+        Write-Host "Incorrect!";
+    }
 }
+
+question;
